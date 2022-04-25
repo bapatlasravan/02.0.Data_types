a1b9bb3f4bb740e0e7406474fef79e7a1cc268af
bapatlasravan <103932090+bapatlasravan@users.noreply.github.com>
2022-04-25 18:59:13 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,18 @@
+"""
+Day 02 Part 01
+Date: 19th April 2022 Tuesday
+Name: Bapatla Sravan
+File Desc: DATA TYPES int;float;str;bool
+"""
+print(type(3249)) #int
+print(type(-3249)) #int
+print(type(0.3249)) #float
+print(type(-0.3249)) #float
+print(type('Welcome to Python Programming language')) #str
+print(type("Welcome to Python Programming language")) #str
+print(type('''Welcome to Python Programming language''')) #str
+print(type("""Welcome to Python Programming language""")) #str
+print(type(14<6)) #bool
+print(type(9<7)) #bool
+print(type(True)) #bool
+print(type(False)) #bool
